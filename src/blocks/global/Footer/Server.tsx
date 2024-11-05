2613a6626100790c222889dcf8cc949a214ae4a7
Jordan <jordanburch101@gmail.com>
2024-11-05 17:51:30 +1300

    fixed footer types

--- src/blocks/global/Footer/Server.tsx
+++ src/blocks/global/Footer/Server.tsx
@@ -3,11 +3,26 @@ import config from "@/payload.config"
 import Image from "next/image"
 import Link from "next/link"
 
+// Add these types
+type FooterType = {
+    logo: {
+      url: string
+      alt: string
+      width: number
+      height: number
+    }
+    links: Array<{
+      url: string
+      label: string
+    }>
+    copyright: string
+}
+
 export default async function FooterServer() {
     const payload = await getPayloadHMR({config})
     const footer = await payload.findGlobal({
         slug: 'footer',
-    })
+    }) as FooterType
     return (
         <section className="bg-slate-900 mt-auto px-4">
             <div className="flex items-center w-full h-32 gap-11">
