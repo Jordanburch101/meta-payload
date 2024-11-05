7c140d16bfc89582d779548371fa0e11eff5f583
Jordan <jordanburch101@gmail.com>
2024-11-06 10:53:02 +1300

    media test

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -62,7 +62,7 @@ export default buildConfig({
         },
         region: process.env.S3_REGION || '',
         endpoint: process.env.S3_ENDPOINT || '',
-        forcePathStyle: true,
+        forcePathStyle: false,
       },
     }),
   ],
