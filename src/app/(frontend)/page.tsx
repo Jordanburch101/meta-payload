77a63e1c935da955fda52c3c31645fa22c3d1c88
Jordan <jordanburch101@gmail.com>
2024-11-11 01:09:36 +1300

    fixed page props

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,23 +1,5 @@
 import PageTemplate, { generateMetadata } from './[slug]/page'
 
-type Args = {
-  params: {
-    slug?: string
-  }
-  searchParams?: { [key: string]: string | string[] }
-}
-
-// Enable caching with revalidation every hour
-export const revalidate = 3600
-
-// Create a wrapper component to handle the home page case
-export default async function HomePage(props: Args) {
-  return PageTemplate({
-    ...props,
-    params: {
-      slug: 'home'
-    }
-  })
-}
+export default PageTemplate
 
 export { generateMetadata }
