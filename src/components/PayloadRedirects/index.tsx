e5c1015e7355650eaf681dcc9210a6a967d24917
Jordan <jordanburch101@gmail.com>
2024-11-11 21:55:11 +1300

    fix: fixed 404 loop

--- src/components/PayloadRedirects/index.tsx
+++ src/components/PayloadRedirects/index.tsx
@@ -16,33 +16,43 @@ export const PayloadRedirects: React.FC<Props> = async ({ disableNotFound, url }
 
   const redirects = await getCachedRedirects()()
 
+  if (!redirects?.length) {
+    if (disableNotFound) return null
+    notFound()
+  }
+
   const redirectItem = redirects.find((redirect) => redirect.from === slug)
 
-  if (redirectItem) {
-    if (redirectItem.to?.url) {
+  if (!redirectItem) {
+    if (disableNotFound) return null
+    notFound()
+  }
+
+  if (redirectItem.to?.url) {
+    if (redirectItem.to.url !== url) {
       redirect(redirectItem.to.url)
     }
-
-    let redirectUrl: string
-
-    if (typeof redirectItem.to?.reference?.value === 'string') {
-      const collection = redirectItem.to?.reference?.relationTo
-      const id = redirectItem.to?.reference?.value
-
-      const document = (await getCachedDocument(collection, id)()) as Page 
-      redirectUrl = `${redirectItem.to?.reference?.relationTo !== 'pages' ? `/${redirectItem.to?.reference?.relationTo}` : ''}/${
-        document?.slug
-      }`
-    } else {
-      redirectUrl = `${redirectItem.to?.reference?.relationTo !== 'pages' ? `/${redirectItem.to?.reference?.relationTo}` : ''}/${
-        typeof redirectItem.to?.reference?.value === 'object'
-          ? redirectItem.to?.reference?.value?.slug
-          : ''
-      }`
-    }
-
-    if (redirectUrl) redirect(redirectUrl)
   }
+
+  let redirectUrl: string
+
+  if (typeof redirectItem.to?.reference?.value === 'string') {
+    const collection = redirectItem.to?.reference?.relationTo
+    const id = redirectItem.to?.reference?.value
+
+    const document = (await getCachedDocument(collection, id)()) as Page 
+    redirectUrl = `${redirectItem.to?.reference?.relationTo !== 'pages' ? `/${redirectItem.to?.reference?.relationTo}` : ''}/${
+      document?.slug
+    }`
+  } else {
+    redirectUrl = `${redirectItem.to?.reference?.relationTo !== 'pages' ? `/${redirectItem.to?.reference?.relationTo}` : ''}/${
+      typeof redirectItem.to?.reference?.value === 'object'
+        ? redirectItem.to?.reference?.value?.slug
+        : ''
+    }`
+  }
+
+  if (redirectUrl) redirect(redirectUrl)
 
   if (disableNotFound) return null
 
