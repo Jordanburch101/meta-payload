6e25301f1acbfe5954763c98d673849a625f0fe8
Jordan <jordanburch101@gmail.com>
2024-11-14 00:28:15 +1300

    fix: commented out loaders

--- src/app/(frontend)/posts/[slug]/loading.tsx
+++ src/app/(frontend)/posts/[slug]/loading.tsx
@@ -1,22 +1,20 @@
 "use client";
-import { tailspin } from 'ldrs'
+// import { tailspin } from 'ldrs'
 
-tailspin.register()
+// tailspin.register()
 
 export default function Loader() {
   return (
     <section className="flex flex-col items-center justify-center min-h-screen">
       <div className="flex flex-col items-center justify-center">
         <div className="grid place-items-center">
-          {/* @ts-ignore */}
-          <l-tailspin
+          {/* <l-tailspin
             size="150"
             stroke="5"
             speed="0.9" 
             color="rgb(255 255 255)"
           >
-          {/* @ts-ignore */}
-          </l-tailspin>
+          </l-tailspin> */}
         </div>
       </div>
     </section>
