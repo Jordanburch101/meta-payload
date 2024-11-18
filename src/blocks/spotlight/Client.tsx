cb19e4c98023b2aa59da7cfef1d2c6ae42ce0d25
Jordan <jordanburch101@gmail.com>
2024-11-18 16:21:53 +1300

    bug: fixed issue with spotlight not showing

--- src/blocks/spotlight/Client.tsx
+++ src/blocks/spotlight/Client.tsx
@@ -4,12 +4,12 @@ import { Spotlight } from "@/components/ui/spotlight";
 
 export function SpotlightPreview({title, description}: {title: string, description: string}) {
   return (
-    <div className="w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
-      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full py-10 md:py-20">
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full py-10 md:py-00">
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           {title}
         </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,21 +22,6 @@ module.exports = {
       },
     },
     extend: {
-      animation: {
-        spotlight: "spotlight 2s ease .75s 1 forwards",
-      },
-      keyframes: {
-        spotlight: {
-          "0%": {
-            opacity: 0,
-            transform: "translate(-72%, -62%) scale(0.5)",
-          },
-          "100%": {
-            opacity: 1,
-            transform: "translate(-50%,-40%) scale(1)",
-          },
-        },
-      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -91,8 +76,19 @@ module.exports = {
             transform: "translate(calc(-50% - 0.5rem))",
           },
         },
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
       },
       animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
